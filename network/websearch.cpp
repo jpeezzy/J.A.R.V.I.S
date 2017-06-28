d5fe0d01d384b12bb2f4f5c0313e7937ad7a6ff6
jpeezzy <justidl3@uci.edu>
2017-06-28 00:36:51 -0700

    example code with wikipedia

--- network/websearch.cpp
+++ network/websearch.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <algorithm> //std::copy_if
 #include <vector>
+#include <fstream>
 //we'll need to implement a callback function here (unavoidable) (sending a function pointer as an arg. for another function. 
 #include <string> 
 size_t CurlWrite_CallbackFunc_StdString(void *contents,size_t size,size_t nmemb, std::string *s)
@@ -25,6 +26,16 @@ size_t CurlWrite_CallbackFunc_StdString(void *contents,size_t size,size_t nmemb,
 	std::copy(first,last,s->begin()+oldLength);
 	return size*nmemb;
 }
+
+void storeInformation(std::string name,std::string information)
+{
+	std::string textName = "infoFiles/" + name + ".txt";
+	char* textNameReal = (char*)textName.c_str();
+	std::ofstream myfile(textNameReal);
+	myfile << information;
+	myfile.close();
+
+}
 //https://stackoverflow.com/questions/2329571/c-libcurl-get-output-into-a-string
 int main(void) {
 	std::string s;
@@ -32,8 +43,8 @@ int main(void) {
 	CURLcode result; //will give you error code if getting informaiton fails
 	curl = curl_easy_init();
 
-//	curl_easy_setopt(curl, CURLOPT_URL, "https://en.wikipedia.org/wiki/CURL?%20action=render");
-	curl_easy_setopt(curl, CURLOPT_URL, "https://www.google.com/search?q=curl&oq=curl+&aqs=chrome..69i57j69i60l5.1389j0j7&client=ubuntu&sourceid=chrome&ie=UTF-8");
+	curl_easy_setopt(curl, CURLOPT_URL, "https://en.wikipedia.org/wiki/CURL?%20action=render");
+	//curl_easy_setopt(curl, CURLOPT_URL, "https://www.google.com/search?q=curl&oq=curl+&aqs=chrome..69i57j69i60l5.1389j0j7&client=ubuntu&sourceid=chrome&ie=UTF-8");
 	curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L); //verify for https://
 	curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L);//verify for https://
 	curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, CurlWrite_CallbackFunc_StdString);	
@@ -61,11 +72,29 @@ int main(void) {
 			}
 			s.erase(i,1); size--;
 		}
+		else if(s.at(i) == '(')
+		{
+			while(s.at(i) != ')')
+			{
+				//std::cout <<"gets to here" <<std::endl;
+				s.erase(i,1); size--;
+			}
+			s.erase(i,1); size--; 
+		}
 		else
 			i++;
 	}
+	//now we need to trim down the search (to the intro paragraph of the wikipedia article);	
+	size_t position = s.find("cURL  is" , 0);
+	size_t position2 = s.find_first_of('\n', position);
+	s.erase(position2, std::string::npos);
+	s.erase(0,position);
+	int x = s.size();
+	std::cout <<"position 2 is at " << position2 <<std::endl;
 	std::cout << s << std::endl; //maybe send s to an html file?
+	storeInformation("cURL",s);
 	std::cout <<"PROGRAM DONE" <<std::endl;
 	//now separate the strings maybe using recursion ??? for loop seems better (remove tags)
+	//std::cout << "Position is at "<< position << std::endl;
 	return 0;
 }
