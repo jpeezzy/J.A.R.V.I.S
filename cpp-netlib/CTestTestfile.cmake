967efdfa26b4ff6c2c4e517440fe8f4d77d92e75
jpeezzy <justidl3@uci.edu>
2017-06-27 17:33:08 -0700

    added networking (basic)

--- cpp-netlib/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/masa/jarvis/cpp-netlib-0.12.0-final
-# Build directory: /home/masa/jarvis/cpp-netlib-0.12.0-final
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
